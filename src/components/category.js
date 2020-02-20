af4801c3785e36984297d0e4c8726eb3b1807f57
narteysarso <narteysarso@gmail.com>
2020-02-20 13:53:35 +0000

    updated category component

--- src/components/category.js
+++ src/components/category.js
@@ -3,12 +3,14 @@ import React from "react";
 function Category(props){
 
     return (
-        <div className="col-sm-12">
-            <h4>{props.name}</h4>
-            <p>{props.description}</p>
-            <div className="row justify-content-right">
-                <a className="btn btn-primary">Edit</a>
-                <a className="btn btn-danger">Remove</a>
+        <div className="col-sm-12" style={{display:"flex", justifyContent:"space-around"}}>
+            <div>
+                <h4>{props.name}</h4>
+                <p>{props.description}</p>
+            </div>
+            <div className="justify-content-right">
+                <a className="btn btn-primary btn-sm">Edit</a>
+                <a className="btn btn-danger btn-sm">Remove</a>
             </div>
         </div>
     )
